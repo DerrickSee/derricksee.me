35103733df67a487161c9acc742d652033da7557
derrick <derrick.see@thatgreenspace.com.sg>
2017-04-26 10:32:54 +0800

    Move whitenoise compression to production file

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -14,6 +14,15 @@ ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 try:
     from .local import *
 except ImportError:
